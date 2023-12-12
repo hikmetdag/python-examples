aca022f88111ac8cd9777b17b1499ef15303db27
hikmetdag <temkihgad@gmail.com>
2023-12-12 21:27:03 +0100

    q7

---
+++ question7.py
@@ -0,0 +1,5 @@
+# Exercise 7: Return the count of a given substring from a string
+# Write a program to find how many times substring “Emma” appears in the given string.
+str_x = "Emma is good developer. Emma is a writer"
+ccount = str_x.count("Emma")
+print(ccount)
